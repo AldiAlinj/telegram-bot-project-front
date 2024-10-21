b7334c89e08777a2e2e0edca8bcd9933d2909c3f
AldiAlinj <aldialinj0@gmail.com>
2024-10-21 13:35:12 +0200

    test timeout and scale

--- src/App.js
+++ src/App.js
@@ -263,24 +263,24 @@ const App = () => {
   };
 
 
-  // if (!isTelegram) {
-  //   return (
-  //     <div
-  //       className={`d-flex  justify-content-center align-items-center`}
-  //       style={{ height: "100vh", width: "100vw" }}
-  //     >
-  //       <div className="d-flex flex-column align-items-center justify-content-center gap-2">
-  //         <h1 className="use-telegram-title mb-0">Page available on</h1>
-  //         <a
-  //           href="https://t.me/AldiTestBot_bot/AldiTestBot"
-  //           className="use-telegram-title"
-  //         >
-  //           Telegram
-  //         </a>
-  //       </div>
-  //     </div>
-  //   );
-  // }
+  if (!isTelegram) {
+    return (
+      <div
+        className={`d-flex  justify-content-center align-items-center`}
+        style={{ height: "100vh", width: "100vw" }}
+      >
+        <div className="d-flex flex-column align-items-center justify-content-center gap-2">
+          <h1 className="use-telegram-title mb-0">Page available on</h1>
+          <a
+            href="https://t.me/AldiTestBot_bot/AldiTestBot"
+            className="use-telegram-title"
+          >
+            Telegram
+          </a>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div
