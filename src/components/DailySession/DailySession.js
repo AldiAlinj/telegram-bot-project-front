e34973b776466da66204302042039ec3e4273b08
AldiAlinj <aldialinj0@gmail.com>
2024-10-28 15:07:59 +0100

    added tooltips

--- src/components/DailySession/DailySession.js
+++ src/components/DailySession/DailySession.js
@@ -6,6 +6,7 @@ import fire from "../../assets/dailySession/fire.png";
 import xMark from "../../assets/xMark.svg";
 import getFormattedNumber from "../../hooks/getFormattedNumber";
 import OutsideClickHandler from "react-outside-click-handler";
+import tooltip from "../../assets/tooltip.svg";
 
 const DailySession = ({
   canClaimToday,
@@ -42,8 +43,8 @@ const DailySession = ({
     },
   ];
 
-
   const [isClicked, setIsClicked] = useState(false);
+  const [tooltipInfo, setTooltipInfo] = useState(false);
 
   const handleClick = () => {
     if (canClaimToday) {
@@ -61,131 +62,163 @@ const DailySession = ({
 
   return (
     <OutsideClickHandler onOutsideClick={onClose}>
-        <div
-      className={`daily-session-outer-wrapper ${
-        show && "daily-session-active"
-      } p-3 d-flex flex-column align-items-center gap-2`}
-    >
-      <div className="daily-session-inner-wrapper d-flex flex-column gap-4 align-items-center justify-content-end position-relative">
-        <div
-          className="d-flex align-items-center justify-content-end w-100 px-2"
-          style={{ position: "absolute", top: "10px" }}
-        >
-          <img src={xMark} onClick={onClose} alt="" style={{cursor: 'pointer'}}/>
-        </div>
-        <div className="daily-session-fire-wrapper w-100 d-flex justify-content-center align-items-center">
-          <img src={fire} alt="" className="fire-img" />
-        </div>
-        <div className="daily-session-lower-wrapper d-flex flex-column gap-2">
-          <div className="d-flex flex-column gap-2 align-items-center">
-            <h6 className="mb-0 daily-streak-amount">{streakDay}</h6>
-            <span className="daily-streak-span">Daily Streak</span>
-            <p className="daily-streak-desc mb-0">
-              Earn points by logging into the game daily
-            </p>
-          </div>
-          <div className="row mx-0 w-100">
-            {days.map((day, index) => (
-              <div className="col-4 px-1" key={index}>
+      <div
+        className={`daily-session-outer-wrapper ${
+          show && "daily-session-active"
+        } p-3 d-flex flex-column align-items-center gap-2`}
+      >
+        <div className="daily-session-inner-wrapper d-flex flex-column gap-4 align-items-center justify-content-end position-relative">
+          <div
+            className="d-flex align-items-center justify-content-between w-100 px-2"
+            style={{ position: "absolute", top: "10px" }}
+          >
+            <div className="position-relative d-flex align-items-center justify-content-center">
+              <img
+                src={tooltip}
+                width={20}
+                height={20}
+                style={{ cursor: "pointer" }}
+                alt=""
+                onClick={() => setTooltipInfo(true)}
+              />
+              <OutsideClickHandler onOutsideClick={() => setTooltipInfo(false)}>
                 <div
-                  className={`daily-streak-item ${
-                    streakDay === index
-                      ? "active-session"
-                      : streakDay > index
-                      ? "completed-session"
-                      : ""
-                  } w-100 px-1 py-2 d-flex flex-column align-items-center gap-1 mb-2`}
+                  className={`daily-session-tooltip-content-wrapper ${
+                    tooltipInfo && "daily-session-tooltip-active"
+                  }`}
                 >
-                  <span
-                    className={`daily-streak-day ${
-                      streakDay === index
-                        ? "active-day"
-                        : streakDay > index
-                        ? "completed-day"
-                        : ""
-                    }`}
-                  >
-                    {day.title}
+                  <span>
+                  <b style={{fontWeight: "800"}}>Daily Reward Streak:</b>
+                  <br />
+                  Earn coins every day by logging in! Keep your streak going to collect bigger rewards. Miss a day, and your streak resets to Day 1. Don’t forget to check in daily for the best rewards!
                   </span>
-                  <div
-                    className={`daily-session-reward-wrapper  ${
-                      streakDay === index
-                        ? "active-reward"
-                        : streakDay > index
-                        ? "completed-reward"
-                        : ""
-                    } px-2 py-1 d-flex align-items-center gap-1`}
-                  >
-                    <img src={coin} width={15} height={15} alt="" />
-                    <span className="daily-session-reward-amount">
-                      +{getFormattedNumber(day.reward, 0)}
-                    </span>
-                  </div>
                 </div>
-              </div>
-            ))}
+              </OutsideClickHandler>
+            </div>
 
-            <div className="col-12 px-1">
-              <div
-                className={`daily-streak-item w-100 ${
-                  streakDay === 6
-                    ? "active-session"
-                    : streakDay > 6
-                    ? "completed-session-7"
-                    : ""
-                } px-1 py-2 d-flex align-items-center justify-content-between gap-1 mb-2`}
-              >
-                <div className="d-flex flex-column align-items-start gap-1 ps-2">
-                  <span
-                    className={`daily-streak-day ${
-                      streakDay === 6
-                        ? "active-day"
-                        : streakDay > 6
-                        ? "completed-day"
-                        : ""
-                    }`}
-                  >
-                    Day 7
-                  </span>
+            <img
+              src={xMark}
+              onClick={onClose}
+              alt=""
+              style={{ cursor: "pointer" }}
+            />
+          </div>
+          <div className="daily-session-fire-wrapper w-100 d-flex justify-content-center align-items-center">
+            <img src={fire} alt="" className="fire-img" />
+          </div>
+          <div className="daily-session-lower-wrapper d-flex flex-column gap-2">
+            <div className="d-flex flex-column gap-2 align-items-center">
+              <h6 className="mb-0 daily-streak-amount">{streakDay}</h6>
+              <span className="daily-streak-span">Daily Streak</span>
+              <p className="daily-streak-desc mb-0">
+                Earn coins by logging into the game daily
+              </p>
+            </div>
+            <div className="row mx-0 w-100">
+              {days.map((day, index) => (
+                <div className="col-4 px-1" key={index}>
                   <div
-                    className={`daily-session-reward-wrapper ${
-                      streakDay === 6
-                        ? "active-reward"
-                        : streakDay > 6
-                        ? "completed-reward"
+                    className={`daily-streak-item ${
+                      streakDay === index
+                        ? "active-session"
+                        : streakDay > index
+                        ? "completed-session"
                         : ""
-                    } px-2 py-1 d-flex align-items-center gap-1`}
+                    } w-100 px-1 py-2 d-flex flex-column align-items-center gap-1 mb-2`}
                   >
-                    <img src={coin} width={15} height={15} alt="" />
-                    <span className="daily-session-reward-amount">
-                      +3,000
+                    <span
+                      className={`daily-streak-day ${
+                        streakDay === index
+                          ? "active-day"
+                          : streakDay > index
+                          ? "completed-day"
+                          : ""
+                      }`}
+                    >
+                      {day.title}
                     </span>
+                    <div
+                      className={`daily-session-reward-wrapper  ${
+                        streakDay === index
+                          ? "active-reward"
+                          : streakDay > index
+                          ? "completed-reward"
+                          : ""
+                      } px-2 py-1 d-flex align-items-center gap-1`}
+                    >
+                      <img src={coin} width={15} height={15} alt="" />
+                      <span className="daily-session-reward-amount">
+                        +{getFormattedNumber(day.reward, 0)}
+                      </span>
+                    </div>
                   </div>
                 </div>
-                <img src={calendar} className="w-25" alt="" />
+              ))}
+
+              <div className="col-12 px-1">
+                <div
+                  className={`daily-streak-item w-100 ${
+                    streakDay === 6
+                      ? "active-session"
+                      : streakDay > 6
+                      ? "completed-session-7"
+                      : ""
+                  } px-1 py-2 d-flex align-items-center justify-content-between gap-1 mb-2`}
+                >
+                  <div className="d-flex flex-column align-items-start gap-1 ps-2">
+                    <span
+                      className={`daily-streak-day ${
+                        streakDay === 6
+                          ? "active-day"
+                          : streakDay > 6
+                          ? "completed-day"
+                          : ""
+                      }`}
+                    >
+                      Day 7
+                    </span>
+                    <div
+                      className={`daily-session-reward-wrapper ${
+                        streakDay === 6
+                          ? "active-reward"
+                          : streakDay > 6
+                          ? "completed-reward"
+                          : ""
+                      } px-2 py-1 d-flex align-items-center gap-1`}
+                    >
+                      <img src={coin} width={15} height={15} alt="" />
+                      <span className="daily-session-reward-amount">
+                        +3,000
+                      </span>
+                    </div>
+                  </div>
+                  <img src={calendar} className="w-25" alt="" />
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <button
+          className={`daily-session-button py-3 mb-3 ${
+            !canClaimToday && "cant-claim"
+          } ${isClicked ? "animate-colors" : ""}`}
+          disabled={!canClaimToday}
+          onClick={handleClick}
+        >
+          {loadingClaim ? (
+            <div
+              className="spinner-border spinner-border-sm text-info"
+              role="status"
+            >
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          ) : canClaimToday ? (
+            "Claim"
+          ) : (
+            "Claimed"
+          )}
+        </button>
       </div>
-      <button
-      className={`daily-session-button py-3 mb-3 ${!canClaimToday && "cant-claim"} ${
-        isClicked ? "animate-colors" : ""
-      }`}
-      disabled={!canClaimToday}
-      onClick={handleClick}
-    >
-      {loadingClaim ? (
-        <div className="spinner-border spinner-border-sm text-info" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
-      ) : canClaimToday ? (
-        "Claim"
-      ) : (
-        "Claimed"
-      )}
-    </button>
-    </div>
     </OutsideClickHandler>
   );
 };
--- src/pages/Friends/Friends.js
+++ src/pages/Friends/Friends.js
@@ -1,11 +1,11 @@
-import React, { useEffect } from "react";
-import giftBox from '../../assets/giftBox.png';
-import verified from '../../assets/verified.svg';
+import React, { useEffect, useState } from "react";
+import giftBox from "../../assets/giftBox.png";
+import verified from "../../assets/verified.svg";
 import "./friends.css";
+import infoTooltip from "../../assets/infoTooltip.svg";
+import OutsideClickHandler from "react-outside-click-handler";
 
-const Friends = ({referredUsers, referralCode}) => {
- 
-
+const Friends = ({ referredUsers, referralCode }) => {
   // const dummyFriends = [
   //   {
   //     telegramUsername: "Lorena579",
@@ -45,59 +45,101 @@ const Friends = ({referredUsers, referralCode}) => {
 
   // ]
 
-  const appLink = "https://t.me/AldiTestBot_bot/AldiTestBot"
+  const [tooltipInfo, setTooltipInfo] = useState(false);
+
+  const appLink = "https://t.me/AldiTestBot_bot/AldiTestBot";
   // const referralLink = `${appLink}?url=${appLink}&startapp=${referralCode}`;
 
   useEffect(() => {
-    window.scrollTo(0,0)
-  }, [])
-  
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <div className="container-fluid friends-wrapper d-flex flex-column justify-content-between p-0">
-      <div className={`d-flex flex-column align-items-center justify-content-end gap-3 invite-friends-banner ${referredUsers.length > 0 && "invite-friends-banner-small"} p-3`}>
-        <img src={giftBox} className="gift-box" alt="" />
-      <div className="d-flex wod-friends-wrapper w-100 justify-content-center">
-      </div>
-      <h1 className="invite-friends-title mb-0">
-        Invite friends and get more points
-      </h1>
-      <button
-  className="invite-friends-button py-3"
-  onClick={() => {
-
-    window.Telegram.WebApp.openTelegramLink(`https://t.me/share/url?url=${appLink}?startapp=${referralCode}`);
-  }}
-  style={{ textDecoration: "none" }}
->
-  Invite Friends
-</button>
-      </div>
-      {referredUsers.length > 0 &&
-      <>
-      <h6 className="friends-list-title mb-0 my-3 mx-3">
-        {referredUsers.length} Friends
-      </h6>
-        <div className="d-flex flex-column friends-list-wrapper mx-3 mb-3">
-          {referredUsers.map((friend, index) => (
-            <div className="friend-item p-1 d-flex align-items-center justify-content-between" key={index}>
-                <div className="d-flex align-items-center gap-2">
-              <div className="friend-initial-wrapper">
-                <span className="friend-initial">{friend.telegramUsername.slice(0, 1)}</span>
+      <div
+        className={`d-flex flex-column align-items-center justify-content-end gap-3 invite-friends-banner position-relative ${
+          referredUsers.length > 0 && "invite-friends-banner-small"
+        } p-3`}
+      >
+        <div className="friends-tooltip-position">
+          <div className="position-relative d-flex align-items-center justify-content-center">
+            <img
+              src={infoTooltip}
+              width={20}
+              height={20}
+              style={{ cursor: "pointer" }}
+              alt=""
+              onClick={() => setTooltipInfo(true)}
+            />
+            <OutsideClickHandler onOutsideClick={() => setTooltipInfo(false)}>
+              <div
+                className={`daily-session-tooltip-content-wrapper ${
+                  tooltipInfo && "daily-session-tooltip-active"
+                }`}
+              >
+                <span>
+                  <b style={{ fontWeight: "800" }}>Friends Referral:</b>
+                  <br />
+                  Invite friends to earn coins! The more friends you invite, the
+                  more you earn. Plus, get extra coins when your invited friends
+                  are verified users. Start inviting and boost your rewards!
+                </span>
               </div>
-                 <div className="d-flex align-items-center gap-1">
-                 <span className="friend-name">{friend.telegramUsername}</span>
-                 {friend.isPremium &&
-                 <img src={verified} width={20} height={20} alt="" />
-                 }
-                 </div>
-                </div>
-                <span className="friend-reward">+{friend.earnedPoints} Coins</span>
-            </div>
-          ))}
+            </OutsideClickHandler>
+          </div>
         </div>
-      </>
-      }
+
+        <img src={giftBox} className="gift-box" alt="" />
+        <div className="d-flex wod-friends-wrapper w-100 justify-content-center"></div>
+        <h1 className="invite-friends-title mb-0">
+          Invite friends and get more points
+        </h1>
+        <button
+          className="invite-friends-button py-3"
+          onClick={() => {
+            window.Telegram.WebApp.openTelegramLink(
+              `https://t.me/share/url?url=${appLink}?startapp=${referralCode}`
+            );
+          }}
+          style={{ textDecoration: "none" }}
+        >
+          Invite Friends
+        </button>
+      </div>
+      {referredUsers.length > 0 && (
+        <>
+          <h6 className="friends-list-title mb-0 my-3 mx-3">
+            {referredUsers.length} Friends
+          </h6>
+          <div className="d-flex flex-column friends-list-wrapper mx-3 mb-3">
+            {referredUsers.map((friend, index) => (
+              <div
+                className="friend-item p-1 d-flex align-items-center justify-content-between"
+                key={index}
+              >
+                <div className="d-flex align-items-center gap-2">
+                  <div className="friend-initial-wrapper">
+                    <span className="friend-initial">
+                      {friend.telegramUsername.slice(0, 1)}
+                    </span>
+                  </div>
+                  <div className="d-flex align-items-center gap-1">
+                    <span className="friend-name">
+                      {friend.telegramUsername}
+                    </span>
+                    {friend.isPremium && (
+                      <img src={verified} width={20} height={20} alt="" />
+                    )}
+                  </div>
+                </div>
+                <span className="friend-reward">
+                  +{friend.earnedPoints} Coins
+                </span>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
